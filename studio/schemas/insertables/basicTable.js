95c0be3c64538386b9a465370e6ca25cac4b3a5e
sc-searchengineunity <sharon@searchengineunity.com>
2023-01-25 14:09:10 -0500

    adding sticky first table function to smart and basic table

--- studio/schemas/insertables/basicTable.js
+++ studio/schemas/insertables/basicTable.js
@@ -28,6 +28,14 @@ export default {
       },
     },
     {
+      name: 'fixedFirstColumn',
+      type: 'boolean',
+      title: 'This table has a fixed first column',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+    {
       name: 'minWidth',
       type: 'string',
       title: 'Minimum Table Width',
